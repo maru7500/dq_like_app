f699b786d1a4a06218bc34c24add1c9596c06ca9
maru7500 <appleapple7500>
2020-03-02 12:50:03 +0900

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root 'games#index'
+
+  get '/start', to: 'games#start'
+  get '/battle', to: 'games#battle'
+
+  resources :braves, only: :update
 end
